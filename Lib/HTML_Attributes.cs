cf82a7d498e0cd6de42adb60619f39a8b54cd957
Alexander Böhm <alex@alebo.de>
2021-09-17 05:50:57 +0200

    Für Attribute ist nun die Datei HTML_Attributes.cs vorgesehen. Für HTML Tags ist die Datei HTML_Tags.cs vorgesehen. Bestehende Methoden wurden in die entsprechenden Dateien ausgelagert.

---
+++ Lib/HTML_Attributes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorDSL {
+    public static partial class Html {
+        public static Attribute className(string className)
+            => new Attribute("class", className);
+
+        public static Attribute href(string value)
+            => new Attribute("href", value);
+
+        public static Attribute target(string value)
+            => new Attribute("target", value);
+    }
+}
--- Lib/Html.cs
+++ Lib/Html.cs
@@ -8,20 +8,7 @@ using System.Linq;
 
 namespace BlazorDSL {
     public static partial class Html {
-        #region Tags
-        public static Node div(Attribute[] attributes, params Node[] inner)
-            => new TagNode("div", attributes, inner);
-
-        public static Node div(params Node[] inner) => new TagNode("div", inner);
-        public static Node div(IEnumerable<Node> inner) => new TagNode("div", inner.ToArray());
-        public static Node h1(string text) => new TagNode("h1", Html.text(text));
-        public static Node text(string text) => new TextNode(text);
-        public static Node p(string text) => new TagNode("p", Html.text(text));
-        public static Node button(string text) => new TagNode("button", Html.text(text));
-        public static Node button(Attribute[] attributes, string text)
-            => new TagNode("button", attributes, Html.text(text));
-        public static Node a(Attribute[] attributes, params Node[] inner)
-            => new TagNode("a", attributes, inner);
+        #region Tags        
 
         public static Node empty() => EmptyNode.Instance;
 
@@ -36,15 +23,6 @@ namespace BlazorDSL {
 
         public static Attribute parameter(string name, object value)
             => new Attribute(name, value);
-
-        public static Attribute className(string className)
-            => new Attribute("class", className);
-
-        public static Attribute href(string value)
-            => new Attribute("href", value);
-
-        public static Attribute target(string value)
-            => new Attribute("target", value);
 
         #endregion        
 
