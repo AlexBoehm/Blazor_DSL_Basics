f5f798d524ec48cfadc2694006f925102ca4d236
Alexander Böhm <alex@alebo.de>
2021-10-15 06:23:59 +0200

    Todo Page mit MVU Muster umgesetzt

--- BlazorDSL/Pages/Todo.cs
+++ BlazorDSL/Pages/Todo.cs
@@ -2,44 +2,64 @@ using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
 using static BlazorDSL.Html;
 using System.Linq;
+using System.Collections.Immutable;
+using Microsoft.AspNetCore.Components.Rendering;
+using System.Diagnostics;
 
-namespace BlazorDSL.Pages {
+namespace BlazorDSL.Pages {    
+
     [Route("/todo")]
-    public partial class TodoPage : WebComponent {
-        List<TodoItem> todoItems = new List<TodoItem>() {
-            new TodoItem { Text = "Task 1", Done = true},
-            new TodoItem { Text = "Task 2", Done = false },
-            new TodoItem { Text = "Task 3", Done = false }
+    public partial class TodoPage : ComponentBase {
+        delegate void Dispatch(Message message);
+
+        static List<TodoItem> todoItems => new List<TodoItem>() {
+            new TodoItem("Task 1", true),
+            new TodoItem("Task 2", false),
+            new TodoItem ("Task 3", false)
         };
 
-        string inputText = "";
+        State _state;
+        Dispatch _dispatch;
 
-        protected override Node Render() =>
+        public TodoPage() {
+            _state = Init();
+            _dispatch = (Message msg) => {
+                Debug.WriteLine(msg);
+                _state = Update(_state, msg);
+            };
+        }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder) {
+            var node = View(_state, _dispatch, this);
+            Renderer.Render(builder, node);
+        }
+
+        static Node View(State state, Dispatch dispatch, object @this) =>
             div(
                 attrs(
                     className("TodoList")
                 ),
 
                 ul(
-                    from item in todoItems
+                    from item in state.todoItems
                     select li(
                         input(
                             type("checkbox"),
                             bind.@checked(
-                                this,
+                                @this,
                                 item.Done,
-                                status => SetItemStatus(item, status)
+                                status => dispatch(new SetItemStatus(item, status))
                             )
                         ),
                         input(
                             type("text"),
                             item.Done ? style("text-decoration: line-through") : emptyAttribute(),
-                            bind.change.@string(this, item.Text, nv => item.Text = nv)
+                            bind.change.@string(@this, item.Text, nv => dispatch(new ChangeItemText(item, nv)))
                         ),
                         button(
                             attrs(
                                 className("Delete"),
-                                onClick(this, _ => todoItems.Remove(item))
+                                onClick(@this, _ => dispatch(new RemoveItem(item)))
                             ),
                             text("x")
                         )
@@ -49,12 +69,12 @@ namespace BlazorDSL.Pages {
                 div(
                     form(
                         attrs(
-                            onSubmit(this, _ => AddToDoItem())
+                            onSubmit(@this, _ => dispatch(new AddItem(state.inputText)))
                         ),
                         input(
                             attrs(
                                 type("text"),
-                                bind.change.@string(this, inputText, nv => inputText = nv)
+                                bind.change.@string(@this, state.inputText, nv => dispatch(new SetInputText(nv)))
                             )
                         ),
                         button(
@@ -76,7 +96,7 @@ namespace BlazorDSL.Pages {
                     ),
                     button(
                         attrs(
-                            onClick(this, x => RemoveDoneItems()),
+                            onClick(@this, x => dispatch(new RemoveDoneItems())),
                             className("RemoveDone")
                         ),
                         "remove all done"
@@ -84,25 +104,64 @@ namespace BlazorDSL.Pages {
                 )
             );
 
-        void AddToDoItem() {
-            todoItems.Add(new TodoItem {
-                Done = false,
-                Text = inputText
-            });
+        record Message();
 
-            inputText = "";
-        }
+        record AddItem(string text) : Message;
+        record RemoveItem(TodoItem item) : Message;
+        record SetItemStatus(TodoItem item, bool status) : Message;
+        record ChangeItemText(TodoItem item, string text) : Message;
+        record RemoveDoneItems() : Message;
+        record SetInputText(string text) : Message;
 
-        void SetItemStatus(TodoItem item, bool status)
-            => item.Done = status;
+        record State(ImmutableList<TodoItem> todoItems, string inputText);
 
-        void RemoveDoneItems() {
-            todoItems.RemoveAll(x => x.Done);
-        }
+        record TodoItem(
+            string Text,
+            bool Done
+        );
 
-        class TodoItem {
-            public string Text { get; set; }
-            public bool Done { get; set; }
-        }
+        static State Init() =>
+            new State(
+                todoItems: ImmutableList<TodoItem>.Empty.AddRange(todoItems),
+                inputText: ""
+            );
+
+        static State Update(State state, Message message) =>
+            message switch {
+                AddItem cmd => state with { 
+                    todoItems = state.todoItems.Add(
+                        new TodoItem(cmd.text, false)
+                    )
+                },
+
+                RemoveItem cmd => state with { 
+                    todoItems = state.todoItems.Remove(cmd.item)
+                },
+
+                RemoveDoneItems cmd => state with {
+                    todoItems = state.todoItems.RemoveAll(
+                        item => item.Done == true
+                    )
+                },
+
+                SetItemStatus cmd => state with { 
+                    todoItems = state.todoItems.Replace(
+                        cmd.item,
+                        new TodoItem(cmd.item.Text, cmd.status)
+                    )
+                },
+
+                ChangeItemText cmd => state with
+                {
+                    todoItems = state.todoItems.Replace(
+                        cmd.item,
+                        new TodoItem(cmd.text, cmd.item.Done)
+                    )
+                },
+
+                SetInputText cmd => state with {
+                    inputText = cmd.text
+                }
+            };
     }
 }
