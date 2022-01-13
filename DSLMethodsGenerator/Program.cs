d528253ce6b6b4cc2825cdb7c0313cc25d738d91
Alexander Böhm <alexander.boehm@imageprofessionals.com>
2022-01-13 05:29:36 +0100

    Neues Projekt DSLMethodsGenerator mit Listen aller HTML Tags, aller HTML void Tags, aller HTML Attribute sowie einer Liste aller Blazor Event Handler. Die Liste der Blazor Event Handler wird über Reflections erzeugt.

---
+++ DSLMethodsGenerator/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DSLMethodsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            ExportBlazorEvents();
+        }
+        
+        private static void ExportBlazorEvents() {
+            IEnumerable<EventHandlerAttribute> attributes = ReadEventHandlerAttributes();
+
+            File.WriteAllLines(
+                "Blazor Events.txt",
+                from item in attributes
+                select $"{item.AttributeName}\t{item.AttributeName}\t{item.EventArgsType}"
+            );
+
+            foreach (var item in attributes) {
+                Console.WriteLine($"{item.AttributeName} {item.EventArgsType}");
+            }
+        }
+
+        private static IEnumerable<EventHandlerAttribute> ReadEventHandlerAttributes() {
+            return typeof(EventHandlers).GetCustomAttributes(
+                typeof(EventHandlerAttribute), true
+            )
+            .Cast<EventHandlerAttribute>();
+        }
+    }
+}
