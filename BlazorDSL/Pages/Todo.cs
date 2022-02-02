ccaa0a54e7174653ad9cb2ad6291b79d1193b5cc
Alexander Böhm <alexander.boehm@imageprofessionals.com>
2022-02-02 05:21:40 +0100

    Neue Formatierung der Datei Pages/Todo.cs mit einer Zeile je Tag

--- BlazorDSL/Pages/Todo.cs
+++ BlazorDSL/Pages/Todo.cs
@@ -8,77 +8,30 @@ namespace BlazorDSL.Pages {
     [Route("/todo")]
     public partial class TodoPage : WebComponent {
         protected override Node Render() =>
-            div(
-                attrs(
-                    className("TodoList")
-                ),
-
+            div( attrs( className("TodoList") ),
                 div(
-                    input(
-                        attrs(
-                            type("text"),
-                            bind.input.@string(this, filterText, value => filterText = value)
-                        )
-                    )
+                    input( attrs( type("text"), bind.input.@string(this, filterText, value => filterText = value)))
                 ),
 
                 ul(
-                    from item in _items
-                    where item.Text.Contains(filterText, StringComparison.OrdinalIgnoreCase)
-                    select li(
-                        input(
-                            type("checkbox"),
-                            bind.change.@checked(
-                                this,
-                                item.Done,
-                                status => item.Done = status
-                            )
-                        ),
-                        input(
-                            type("text"),
-                            item.Done ? style("text-decoration: line-through") : emptyAttribute(),
-                            bind.change.@string(this, item.Text, nv => item.Text = nv)
-                        ),
-                        button(
-                            attrs(
-                                className("Delete"),
-                                onClick(this, _ => _items.Remove(item))
-                            ),
-                            text("x")
-                        )
+                    from item in _items where item.Text.Contains(filterText, StringComparison.OrdinalIgnoreCase) select
+                    li (
+                        input( type("checkbox"), bind.change.@checked(this, item.Done, status => item.Done = status)),
+                        input( type("text"), item.Done ? style("text-decoration: line-through") : emptyAttribute(), bind.change.@string(this, item.Text, nv => item.Text = nv)),
+                        button( attrs( className("Delete"), onClick(this, _ => _items.Remove(item)) ), text("x"))
                     )
                 ),
-                
-                form(
-                    attrs(
-                        onSubmit(this, _ => AddItem())
-                    ),
-                    input(
-                        attrs(
-                            type("text"),
-                            bind.change.@string(this, inputText, nv => inputText = nv)
-                        )
-                    ),
+
+                form( attrs( onSubmit(this, _ => AddItem())),
+                    input( attrs( type("text"), bind.change.@string(this, inputText, nv => inputText = nv))),
                     button(
-                        span(
-                            attrs(
-                                className("oi oi-plus"),
-                                attribute("aria-hidden", "true")
-                            )
-                        ),
+                        span( attrs( className("oi oi-plus"), attribute("aria-hidden", "true"))),
                         text("add")
                     )
                 ),
 
-                div(
-                    attrs(
-                        className("ButtonBox")
-                    ),
-                    button(
-                        attrs(
-                            onClick(this, x => RemoveDoneItems()),
-                            className("RemoveDone")
-                        ),
+                div( attrs( className("ButtonBox")),
+                    button( attrs( onClick(this, x => RemoveDoneItems()), className("RemoveDone")),
                         "remove all done"
                     )
                 )
